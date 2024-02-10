5fd6c1a7e381eb3894737553174024a2f4718d82
Mehmet <mehmetkusken@gmail.com>
2024-02-10 11:24:32 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import MovieIndex from "./containers/MovieIndex";
+
+function App() {
+  return (
+        <>
+        <h1>FEED</h1>
+        <MovieIndex/>
+        </>
+     
+   
+  );
+}
+
+export default App;
---
+++ src/containers/MovieIndex.js
@@ -0,0 +1,5 @@
+function MovieIndex(props){
+   return<h1>Movie Go Here</h1>
+}
+
+export default MovieIndex 
